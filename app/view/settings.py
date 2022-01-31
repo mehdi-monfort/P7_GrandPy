91994c64af33c14eeb11a06e09396dc24dfc0f87
mehdi monfort <mehdimonfort@gmail.com>
2022-01-31 18:23:53 +0100

    removal of dotenv

--- app/view/settings.py
+++ app/view/settings.py
@@ -1,7 +1,5 @@
 import os
-from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
 
 API_KEY = os.getenv("MAPS")
 GEOCODE_BASE_URL = "https://geocode.search.hereapi.com/v1/geocode"
