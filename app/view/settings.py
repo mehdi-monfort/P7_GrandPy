5233f97e4032dd31daba3017dc85d90240038599
mehdi monfort <mehdimonfort@gmail.com>
2022-02-24 21:44:14 +0100

    add mock test

--- app/view/settings.py
+++ app/view/settings.py
@@ -1,5 +1,7 @@
 import os
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 
 API_KEY = os.getenv("MAPS")
 GEOCODE_BASE_URL = "https://geocode.search.hereapi.com/v1/geocode"
--- test_pytest.py
+++ test_pytest.py
@@ -1,39 +1,41 @@
 # coding: utf-8
 import pytest
+
+from app.view import maps
 from app.view.maps import Maps
 from app.view.sortword import Sortword
 from app.view.wiki import Wiki
 
 
-def test_geocode():
+def test_geocode(monkeypatch):
+    def mockreturn():
+        return {'lat': 48.85717, 'lng': 2.3414}
 
-	address = "paris"
-	assert Maps.geocode(address) == {'lat': 48.85717, 'lng': 2.3414}
+    address = "paris"
+    monkeypatch.setattr(maps, 'Maps', mockreturn)
+    expected_value = {'lat': 48.85717, 'lng': 2.3414}
+    assert Maps.geocode(address) == expected_value
 
 
 class TestSortword:
 
-	def setup_method(self):
-		self.S = Sortword()
+    def setup_method(self):
+        self.S = Sortword()
 
-	def test_regword1(self):
+    def test_regword1(self):
+        self.input = "ou se situe la tour eiffel ?"
+        assert self.S.regword(self.input) == ['tour', 'eiffel']
 
-		self.input = "ou se situe la tour eiffel ?"
-		assert self.S.regword(self.input) == ['tour', 'eiffel']
+    def test_regword2(self):
+        self.input = "qu'y a t'il d'intéressant sur paris ?"
+        assert self.S.regword(self.input) == ['paris']
 
-	def test_regword2(self):
-
-		self.input = "qu'y a t'il d'intéressant sur paris ?"
-		assert self.S.regword(self.input) == ['paris']
-
-	def test_regword3(self):
-		
-		self.input = "Où se trouve la cathédrale de Quimper ?"
-		assert self.S.regword(self.input) == ['cathédrale', 'quimper']
+    def test_regword3(self):
+        self.input = "Où se trouve la cathédrale de Quimper ?"
+        assert self.S.regword(self.input) == ['cathédrale', 'quimper']
 
 
 def test_extract():
-
-	longitude = "48.85824"
-	latitude = "2.2945"
-	assert Wiki.extract(longitude, latitude) == 'Le Jules Verne est un restaurant parisien situé au deuxième étage de la tour Eiffel et spécialisé en cuisine française classique.'
+    longitude = "48.85824"
+    latitude = "2.2945"
+    assert Wiki.extract(longitude, latitude) == 'Le Jules Verne est un restaurant parisien situé au deuxième étage de la tour Eiffel et spécialisé en cuisine française classique.'
